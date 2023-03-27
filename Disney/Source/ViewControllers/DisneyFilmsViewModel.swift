13561cbea0ffbb379f25b55483b03d749fb7e911
Rogerio Barreira <abacuque37@hotmail.com>
2023-03-27 16:54:29 -0300

    Implemented scrowInfinit

--- Disney/Source/ViewControllers/DisneyFilmsViewModel.swift
+++ Disney/Source/ViewControllers/DisneyFilmsViewModel.swift
@@ -12,6 +12,18 @@ class DisneyFilmsViewModel: NSObject {
     private let worker = Worker()
     private var disneyFilms: [Datum] = []
     
+    var loadinRequest = false
+    var currentPage = 0
+    var totalResult = 0
+    
+    func currentPageAdd() {
+        return currentPage = currentPage + 1
+    }
+    
+    var addDisney: [Datum] {
+        return disneyFilms
+    }
+    
     var numberOfRows: Int {
         disneyFilms.count
     }
@@ -25,6 +37,23 @@ class DisneyFilmsViewModel: NSObject {
         worker.workerDisney { disneyFilms, success in
             if success {
                 self.disneyFilms = disneyFilms?.data ?? []
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
+    func requestDisneyAddViewModel(completion: @escaping(Bool) -> Void) {
+        loadinRequest = true
+        worker.workerDisney(pageIn: currentPage) { disneyFilms, success in
+            if success {
+                if let disneyFilmsAdd = disneyFilms {
+                    self.disneyFilms.append(contentsOf: disneyFilmsAdd.data ?? [])
+                    self.totalResult = disneyFilms?.count ?? 0 
+                    print("Total de resultado: \(self.totalResult)")
+                    print("Total já carregados: \(self.disneyFilms.count)")
+                }
                 completion(true)
             } else {
                 completion(false)
--- Disney/Source/Worker/Worker.swift
+++ Disney/Source/Worker/Worker.swift
@@ -12,9 +12,13 @@ class Worker: NSObject {
     private let worker = Request()
     private var disneyFilm: Disney?
     
-    func workerDisney(completion:@escaping(Disney?,Bool)-> Void) {
+    func workerDisney(pageIn: Int = 0, completion:@escaping(Disney?,Bool)-> Void) {
+        let pageDefault = 1
+        let basePath = "https://api.disneyapi.dev/characters?"
+        let page = "\(pageDefault + pageIn)"
+        let url = "\(basePath)page=\(page)"
         
-        worker.request(urlString: "https://api.disneyapi.dev/characters", method: .get, custom: Disney.self) { [weak self] disneyFilm, success in
+        worker.request(urlString: url, method: .get, custom: Disney.self) { [weak self] disneyFilm, success in
             guard let self = self else { return }
             if success {
                 self.disneyFilm = disneyFilm
